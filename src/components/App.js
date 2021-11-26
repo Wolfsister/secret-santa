d953df0486959e21e4a74751650bdc8002e02b7d
Denis CHENESSEAU <denis.chenesseau@gmail.com>
2021-11-26 21:37:59 +0100

    Use previously used users

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,20 @@ import Header from './Header'
 import PlayersList from './PlayersList';
 import LaunchDraw from './LaunchDraw';
 import DrawDisplay from './DrawDisplay';
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 
 function App() {
 
-  // const [listOfPlayers, updateListOfPlayers] = useState([{id: 1, playerName: ''}])
-  
-  const [listOfPlayers, updateListOfPlayers] = useState([{playerName: 'Candice'}, {playerName: 'Denis'}, {playerName: 'Paul'}, {playerName: ''}])
+  const savedListPlayers = localStorage.getItem('savedList')
+  const [listOfPlayers, updateListOfPlayers] = useState(savedListPlayers ? JSON.parse(savedListPlayers) : [{playerName: ''}])
   const [drawList, updateDrawList] = useState([])
   const [drawDisplayIndex, updateDrawDisplayIndex] = useState(0)
 
+  useEffect(() => {
+
+    localStorage.setItem('savedList', JSON.stringify(listOfPlayers))
+
+  }, [listOfPlayers])
 
   return (
     <div className="App">
